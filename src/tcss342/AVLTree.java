761100f6a542c09ef73561d5d25377c370cbfa59
newfortune <chiemeziemnwoke@yahoo.com>
2016-06-16 23:13:23 -0700

    Corrected some rotation bugs

--- src/tcss342/AVLTree.java
+++ src/tcss342/AVLTree.java
@@ -1,74 +1,14 @@
+/*
+ *Nwoke Fortune Chiemeziem
+ *AVLTrees- TCSS Home work 3 
+ */
 package tcss342;
 
 public class AVLTree<T extends Comparable<T>>
 {
-	private static class Node<T>
-	{
-		public T item;
-		// public int height;
-		public Node<T> parent;
-		public Node<T> left;
-		public Node<T> right;
-
-		public Node(T item)
-		{
-			this(item, null);
-		}
-
-		public Node(T item, Node<T> parent)
-		{
-			this.item = item;
-			this.parent = parent;
-		}
-
-		
-		public Node<T> getSuccessor()
-		{
-			if (right != null)
-			{
-				Node<T> successor = right;
-				while (successor.left != null)
-					successor = successor.left;
-				return successor;
-			} else
-			{
-				Node<T> successor = this;
-				while (successor.parent != null
-						&& successor.parent.right == successor)
-					successor = successor.parent;
-				successor = successor.parent;
-				return successor;
-			}
-		}
-
-		/*
-		 * delete (non-Javadoc)
-		 * 
-		 * @see java.lang.Object#toString()
-		 */
-		public String toString()
-		{
-			return item.toString();
-		}
-	}
-
 	private int size;
 
 	private Node<T> root;
-
-	public int getHeight(Node<T> root)
-	{
-		if (root == null)
-		{
-			return -1;
-		} else
-		{
-			int theLeft = getHeight(root.left);
-			int theRight = getHeight(root.right);
-
-			return Math.max(theLeft, theRight) + 1;
-		}
-	}
 
 	public boolean add(T item)
 	{
@@ -91,24 +31,239 @@ public class AVLTree<T extends Comparable<T>>
 			// Add child to the left.
 			if (n.left == null)
 			{
+				// n.height = getheight(n);
 				n.left = new Node<T>(item, n);
+				resetHeight(n.left);
+
 				size++;
-				 //reArrangeTree(n.left);
+
 				return true;
 			} else
+			{
 				return add(n.left, item);
+			}
+
 		else if (cmp < 0)
 			// Add child to the right.
 			if (n.right == null)
 			{
 				n.right = new Node<T>(item, n);
+				resetHeight(n.right);
 				size++;
-				// reArrangeTree(n.right);
+
 				return true;
 			} else
+			{
 				return add(n.right, item);
+			}
+
 		else
 			return false;
+	}
+
+	public void resetHeight(Node<T> node)
+	{
+		while (node.parent != null)
+		{
+
+			if (node.parent.left == null && node.parent.right == node)
+			{
+				node.parent.height += 1;
+
+				if (isVoilation(node.parent))
+				{
+
+					// double left right
+					if (node.right == null)
+					{
+						node.parent = doubleLeftRight(node);
+					} else
+					{
+
+						node.parent.parent.right = rotateLeft(node.parent);
+					}
+				}
+			} else if (node.parent.right == null && node.parent.left == node)
+			{
+				node.parent.height += 1;
+				if (isVoilation(node.parent))
+				{
+
+					// double right left
+					if (node.left == null)
+					{
+
+						node.parent.parent.right = doubleRightLeft(node);
+					} else
+					{
+
+						node.parent.parent.left = rotateRight(node.parent);
+					}
+				}
+			}
+			if (node.parent.left != null && node.parent.right != null)
+			{
+				node.parent.height = Math.max(node.parent.left.height,
+						node.parent.right.height) + 1;
+
+				if (isVoilation(node.parent))
+				{
+
+					if (node.parent.left.height - node.parent.right.height == 2)
+					{
+
+						node.right = rotateRootRight(node.parent);
+						continue;
+					} else
+					{
+
+						node.left = rotateRootLeft(node.parent);
+						continue;
+					}
+
+				}
+			}
+			node = node.parent;
+		}
+		root = node;
+
+		if (root.left != null && root.right != null)
+			root.height = Math.max(root.left.height, root.right.height) + 1;
+	}
+
+	private Node<T> rotateRootRight(Node<T> node)
+	{ // node == 54
+		Node<T> a = node;// 70
+		Node<T> b = node.left; // 54
+		Node<T> c = node.left.right; // 55
+
+		b.parent = a.parent;
+		b.right = a;
+		b.left.right = null;
+
+		a.parent = b;
+		a.height -= 2;
+		a.left = c;
+
+		c.parent = a;
+
+		return a;
+	}
+
+	private Node<T> doubleLeftRight(Node<T> node)
+	{
+
+		Node<T> a = node;// 70
+		Node<T> b = node.parent; // 50
+		Node<T> c = node.left; // 55
+
+		a.height += 1;
+		a.parent = b.parent;
+
+		b.height -= 2;
+		b.parent = c;
+		b.right = null;
+
+		c.left = b;
+		c.height += 1;
+
+		return rotateRight(a);
+
+	}
+
+	private Node<T> doubleRightLeft(Node<T> node)
+	{
+
+		Node<T> a = node;// 73
+		Node<T> b = node.parent; // 75
+		Node<T> c = node.right; // 74
+
+		a.height += 1;
+		a.parent = b.parent;
+
+		b.height -= 2;
+		b.parent = c;
+		b.left = null;
+
+		c.right = b;
+		c.height += 1;
+
+		return rotateLeft(a);
+
+	}
+
+	/**
+	 * Rotates node a to the left, making its right child into its parent.
+	 * 
+	 * @param a the former parent
+	 *            
+	 * @return the new parent (formerly a's right child)
+	 */
+	private Node<T> rotateLeft(Node<T> a)
+	{
+		a.height -= 1;
+		Node<T> b = a.right;
+		b.parent = a.parent;
+		a.right = b.left;
+
+		a.height -= 1;
+		b.left = a;
+		a.parent = b;
+		return b;
+
+	}
+
+	private Node<T> rotateRight(Node<T> a)
+	{
+		a.height -= 1;
+		Node<T> b = a.left;
+		b.parent = a.parent;
+
+		a.height -= 1;
+		a.left = b.right;
+
+		b.right = a;
+		a.parent = b;
+
+		return b;
+	}
+
+	private Node<T> rotateRootLeft(Node<T> node)
+	{
+		// node == 54
+		Node<T> a = node;// 70
+		Node<T> b = node.right; // 54
+		Node<T> c = node.right.left; // 55
+
+		b.parent = a.parent;
+		b.left = a;
+		b.right.left = null;
+
+		a.parent = b;
+		a.height -= 2;
+		a.right = c;
+
+		c.parent = a;
+
+		return a;
+	}
+
+	private boolean isVoilation(Node<T> node)
+	{
+		boolean v = false;
+		if (node.left == null && node.right != null
+				&& (node.right.height == 1 || node.right.height == -1))
+			v = true;
+		else if (node.right == null && node.left != null
+				&& (node.left.height == 1 || node.left.height == -1))
+			v = true;
+		else if (node.left != null && node.right != null)
+		{
+			if (node.left.height - node.right.height == -2
+					|| node.left.height - node.right.height == 2)
+				v = true;
+		}
+		return v;
 	}
 
 	public boolean remove(T item)
@@ -136,55 +291,40 @@ public class AVLTree<T extends Comparable<T>>
 				return remove(n.right, item);
 		else
 		{
-			remove(n);
+			Node<T> x;   
+		    
+		    if (n.left == null){  
+		      x = n.right;
+		    }
+		    else if (n.right == null) 
+		      x = n.left;
+		    else {                 
+		      x = n.getSuccessor();  
+		      remove(x.item); 
+		     
+		      x.left = n.left; 
+		      x.right = n.right;
+		      if (x.left != null) x.left.parent = x;
+		      if (x.right != null) x.right.parent = x;
+		    } 
+		    
+		    
+		    if (x != null)
+		      x.parent = n.parent;
+		    if (root == n)
+		      root = x;
+		    else{
+		      if (n == n.parent.left)
+		          n.parent.left = x;
+		      else
+		          n.parent.right = x;
+		    } 
 			size--;
+			resetHeight(n);
 			return true;
 		}
 	}
-
-	private void remove(Node<T> n)
-	{
-		if (n.right == null)
-		{
-			// No right subtree, move left subtree (if any) up to replace n.
-			if (n == root)
-			{
-				root = n.left;
-				if (root != null)
-					root.parent = null;
-			} else
-			{
-				if (n.parent.left == n)
-					n.parent.left = n.left;
-				else
-					n.parent.right = n.left;
-				if (n.left != null)
-					n.left.parent = n.parent;
-			}
-		} else if (n.left == null)
-		{
-			// No left subtree, move right subtree up to replace n.
-			if (n == root)
-			{
-				root = n.right;
-				root.parent = null;
-			} else
-			{
-				if (n.parent.left == n)
-					n.parent.left = n.right;
-				else
-					n.parent.right = n.right;
-				n.right.parent = n.parent;
-			}
-		} else
-		{
-			// Both children are present, replace n with its successor and
-			// remove the successor.
-			Node<T> successor = n.getSuccessor();
-			n.item = successor.item;
-			remove(successor);
-		}
-	}
+	
 
 	public boolean contains(T item)
 	{
@@ -196,7 +336,7 @@ public class AVLTree<T extends Comparable<T>>
 		else
 			return contains(root, item);
 	}
-
+	
 	private boolean contains(Node<T> n, T item)
 	{
 		int cmp = n.item.compareTo(item);
@@ -216,17 +356,17 @@ public class AVLTree<T extends Comparable<T>>
 			return true;
 	}
 
-	public int size()
-	{
-		return size;
-	}
-
 	public void clear()
 	{
 		size = 0;
 		root = null;
 	}
-
+	
+	public int size()
+	{
+		return size;
+	}
+	
 	public void hasValidStructure()
 	{
 		if (root != null)
@@ -249,32 +389,63 @@ public class AVLTree<T extends Comparable<T>>
 			hasValidStructure(n.right);
 		}
 	}
-
-	public String toString()
-	{
-		// System.out.println(root.left);
-		//System.out.println(getHeight(root));
-		StringBuffer sb = new StringBuffer();
-		// for (T data : this)
-		// sb.append(data.toString() + " ");
-
-		return sb.toString();
+	
+	public static void main(final String... args) {
+		
+		Integer[] a = {3,2,8,1,5,9,4,7,6};
+		
+		AVLTree<Integer> m = new AVLTree<>();
+		
+		for (Integer n : a) {
+			m.add(n);
+		}
+		System.out.println(m.size());
 	}
-
-	public static void main(final String[] args)
+	public static class Node<T>
 	{
 
-		// Integer[] a = { 50, 70, 20, 1, 30, 55, 75, 73, 74};
-		Integer[] a = { 70, 75, 77, 79 };
-		// Integer[] a = {11, 6, 8, 19, 4, 10, 5, 17, 43, 49, 31};
+		public T item;
+		public Node<T> right;
+		public Node<T> left;
+		public Node<T> parent;
 
-		AVLTree<Integer> tree = new AVLTree<Integer>();
-		for (Integer n : a)
+		public int height;
+
+		public Node(T item)
 		{
-			tree.add(n);
+			this(item, null);
 		}
 
-		System.out.println(tree);
+		public Node(T item, Node<T> parent)
+		{
+			this.item = item;
+			this.parent = parent;
+		}
+		
+		public Node<T> getSuccessor()
+		{
+			if (right != null)
+			{
+				Node<T> successor = right;
+				while (successor.left != null)
+					successor = successor.left;
+				return successor;
+			} else
+			{
+				Node<T> successor = this;
+				while (successor.parent != null
+						&& successor.parent.right == successor)
+					successor = successor.parent;
+				successor = successor.parent;
+				return successor;
+			}
+		}
+
+		public String toString()
+		{
+			return item + "";
+		}
 
 	}
+
 }
